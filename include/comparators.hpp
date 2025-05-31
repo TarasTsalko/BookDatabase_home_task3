2cc2cccde31200e4d8df2db52776cb37087211bb
Taras Tsalko <you@example.com>
2025-05-31 09:48:21 +0300

    Добавлены компораторы для элементов класса Book

--- include/comparators.hpp
+++ include/comparators.hpp
@@ -1,9 +1,21 @@
 #pragma once
 
 #include "book.hpp"
+#include <string_view>
 
 namespace bookdb::comp {
 
-struct LessByAuthor {};
+struct LessByAuthor {
+    bool operator()(const Book &lhv, const Book &rhv) const { return lhv.author < rhv.author; }
+};
+
+struct LessByPopularity {
+    // используем >, так как поплярные книги должны находится в верху контейнера (списка)
+    bool operator()(const Book &lhv, const Book &rhv) const { return lhv.read_count > rhv.read_count; }
+};
+
+struct LessByRating {
+    bool operator()(const Book &lhv, const Book &rhv) const { return lhv.rating > rhv.rating; }
+};
 
 }  // namespace bookdb::comp
