f76ddf9b4a99fffe1cad702b5dbaedbe2d142c08
Taras Tsalko <you@example.com>
2025-06-01 10:37:52 +0300

    Раскамментирована часть кода в main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 
+#include "book.hpp"
 #include "book_database.hpp"
 #include "comparators.hpp"
 #include "filters.hpp"
@@ -20,9 +21,7 @@ int main() {
     // Create a book database
     BookDatabase<std::vector<Book>> db;
 
-    /*
-
-    Код закомментирован, чтобы не приводить к ошибке компиляции
+    // Код закомментирован, чтобы не приводить к ошибке компиляции
 
     // Add some books
     db.EmplaceBack("1984", "George Orwell", 1949, Genre::SciFi, 4., 190);
@@ -44,31 +43,34 @@ int main() {
     std::sort(db.begin(), db.end(), comp::LessByPopularity{});
     std::print("Books sorted by popularity: {}\n\n==================\n", db);
 
-    // Author histogram
     auto histogram = buildAuthorHistogramFlat(db);
     std::print("Author histogram: {}", histogram);
 
-    // Ratings
     auto genreRatings = calculateGenreRatings(db.begin(), db.end());
     std::print("\n\nAverage ratings by genres: {}\n", genreRatings);
 
     auto avrRating = calculateAverageRating(db);
     std::print("Average books rating in library: {}\n", avrRating);
 
-    // Filters
-    auto filtered = filterBooks(db.begin(), db.end(), all_of(YearBetween(1900, 1999), RatingAbove(4.5)));
-    std::print("\n\nBooks from the 20th century with rating ≥ 4.5:\n");
-    std::for_each(filtered.cbegin(), filtered.cend(), [](const auto &v) { std::print("{}\n", v.get()); });
-
     // Top 3 books
     auto topBooks = getTopNBy(db, 3, comp::LessByRating{});
     std::print("\n\nTop 3 books by rating:\n");
     std::for_each(topBooks.cbegin(), topBooks.cend(), [](const auto &v) { std::print("{}\n", v.get()); });
 
+    std::print("\n\nTop 3 random books:\n");
+    auto randomBooks = sampleRandomBooks(db, 3);
+    std::for_each(randomBooks.cbegin(), randomBooks.cend(), [](const auto &v) { std::print("{}\n", v.get()); });
+
     auto orwellBookIt = std::find_if(db.begin(), db.end(), [](const auto &v) { return v.author == "George Orwell"; });
     if (orwellBookIt != db.end()) {
         std::print("\n\nTransparent lookup by authors. Found Orwell's book: {}\n", *orwellBookIt);
     }
+
+    /*
+    // Filters
+    auto filtered = filterBooks(db.begin(), db.end(), all_of(YearBetween(1900, 1999), RatingAbove(4.5)));
+    std::print("\n\nBooks from the 20th century with rating ≥ 4.5:\n");
+    std::for_each(filtered.cbegin(), filtered.cend(), [](const auto &v) { std::print("{}\n", v.get()); });
     */
 
     return 0;
